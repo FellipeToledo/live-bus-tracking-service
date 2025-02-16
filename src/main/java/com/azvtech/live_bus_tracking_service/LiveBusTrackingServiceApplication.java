5daa4359d6c31206045c1af9757dadab435d3da9
FellipeToledo <fellipetoledo.dev@gmail.com>
2025-02-16 16:29:56 -0300

    Initial commit

---
+++ src/main/java/com/azvtech/live_bus_tracking_service/LiveBusTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.azvtech.live_bus_tracking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBusTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveBusTrackingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azvtech/live_bus_tracking_service/LiveBusTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azvtech.live_bus_tracking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBusTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
